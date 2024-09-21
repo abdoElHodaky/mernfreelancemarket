7c60c3c4bfe213d2447614829caccfe3a9d58c74
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-09-21 12:42:58 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ const connect = async () => {
 app.use(cors(/*{ origin: "http://localhost:5173", credentials: true }*/));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static( 'public'))
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
